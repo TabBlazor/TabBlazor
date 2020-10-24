e917c20a0c2c6548afce9ec5f93050c465f28783
Joakim Dangården <Joakim.Dangarden@swegon.com>
2020-10-24 15:19:41 +0200

    Added Icons

---
+++ src/Tabler.Icons/Extensions.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Tabler.Icons
+{
+    public static class Extensions
+    {
+
+        public static string GetIconName(this TablerIconType iconType)
+        {
+            var iconName = iconType.ToString();
+            if (iconName.StartsWith("_"))
+            {
+                iconName = iconName.Substring(1);
+            }
+            iconName = iconName.Replace("_", "-");
+
+            return iconName.ToLower();
+        }
+
+    }
+}
---
+++ src/Tabler.Icons/TablerIcon.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Tabler.Icons
+{
+    public partial class TablerIcon : ComponentBase
+    {
+        //[Inject] public IconService IconService { get; set; }
+        [Parameter(CaptureUnmatchedValues = true)] public Dictionary<string, object> Attributes { get; set; }
+        [Parameter] public TablerIconType IconType { get; set; }
+        [Parameter] public string Color { get; set; }
+        [Parameter] public int Size { get; set; } = 24;
+        [Parameter] public double StrokeWidth { get; set; } = 2;
+
+    //    private TablerIconType? iconType;
+    //    private string svgElements;
+
+        //protected override async Task OnParametersSetAsync()
+        //{
+        //    if (iconType != IconType || string.IsNullOrWhiteSpace(svgElements))
+        //    {
+        //        svgElements = await IconService.GetIconElementsAsync(IconType.GetIconName());
+        //        iconType = IconType;
+        //    }
+        //}
+    }
+}
