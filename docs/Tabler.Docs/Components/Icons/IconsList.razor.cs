5f6741f2bd48d0427daa9377fc81b5d2593eaff7
Joakim Dangården <Joakim.Dangarden@swegon.com>
2020-10-26 16:48:20 +0100

    Fixed icon list

--- docs/Tabler.Docs/Components/Icons/IconsList.razor.cs
+++ docs/Tabler.Docs/Components/Icons/IconsList.razor.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
@@ -10,29 +11,57 @@ namespace Tabler.Docs.Components.Icons
 {
    public partial class IconsList : ComponentBase
     {
-      
-        //private List<TablerIconType> icons = Enum.GetValues(typeof(TablerIconType)).Cast<TablerIconType>().ToList();
+
+        private List<Icon> icons = new List<Icon>();
+        private List<Icon> filteredIcons = new List<Icon>();
         //private List<TablerIconType> filteredIcons = Enum.GetValues(typeof(TablerIconType)).Cast<TablerIconType>().ToList();
         private int size = 24;
         private double strokeWidth = 2;
         private string searchText;
         private string color;
 
-  
+
+
+        protected override void OnInitialized()
+        {
+            LoadIcons();
+            filteredIcons = icons;
+
+        }
+
+        private void LoadIcons()
+        {
+            var properties = typeof(DemoIcons).GetProperties(BindingFlags.Public | BindingFlags.Static);
+
+            foreach (var property in properties)
+            {
+                var value = property.GetValue(null).ToString();
+                icons.Add(new Icon { Name = property.Name, Elements = value });
+            }
+        }
+
         private void SearchIcons(ChangeEventArgs e)
         {
 
-            //searchText = e.Value.ToString();
-            //if (string.IsNullOrWhiteSpace(searchText))
-            //{
-            //    filteredIcons = icons;
-            //}
-            //else
-            //{
-            //    filteredIcons = icons.Where(x => x.GetIconName().Contains(searchText, StringComparison.InvariantCulture)).ToList();
-            //}
+            searchText = e.Value.ToString();
+            if (string.IsNullOrWhiteSpace(searchText))
+            {
+                filteredIcons = icons;
+            }
+            else
+            {
+                filteredIcons = icons.Where(x => x.Name.Contains(searchText, StringComparison.InvariantCulture)).ToList();
+            }
 
         }
 
     }
+
+
+    public class Icon
+    {
+        public string Name { get; set; }
+        public string Elements { get; set; }
+    }
+
 }
