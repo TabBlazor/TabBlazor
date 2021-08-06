9e48cf8a723c3e01cd51e861285ee1fbcbc08358
Joakim Dangården <Joakim.Dangarden@swegon.com>
2021-08-06 10:14:50 +0200

    copy icon to clipboard

--- docs/Tabler.Docs/Components/Icons/IconsList.razor.cs
+++ docs/Tabler.Docs/Components/Icons/IconsList.razor.cs
@@ -3,14 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
+using TabBlazor.Services;
 
 namespace Tabler.Docs.Components.Icons
 {
-   public partial class IconsList : ComponentBase
+    public partial class IconsList : ComponentBase
     {
+        [Inject] private TablerService tabService { get; set; }
+        [Inject] private ToastService toastService { get; set; }
 
         private List<Icon> icons = new List<Icon>();
         private List<Icon> filteredIcons = new List<Icon>();
+        private List<Icon> selectedIcons = new List<Icon>();
         private int size = 24;
         private double strokeWidth = 2;
         private string searchText;
@@ -44,12 +49,45 @@ namespace Tabler.Docs.Components.Icons
                 filteredIcons = icons.Where(x => x.Name.Contains(searchText, StringComparison.InvariantCultureIgnoreCase)).ToList();
             }
         }
+
+        private bool IsSelected(Icon icon) => selectedIcons.Contains(icon);
+
+        private void SelectIcon(Icon icon)
+        {
+            if (IsSelected(icon))
+            {
+                selectedIcons.Remove(icon);
+            }
+            else
+            {
+                selectedIcons.Add(icon);
+            }
+           
+        }
+
+        private async Task CopyToClipboard()
+        {
+            var iconlist = "";
+            foreach (var icon in selectedIcons)
+            {
+                iconlist += icon.GetStaticProperty() + Environment.NewLine;
+            }
+
+            await tabService.CopyToClipboard(iconlist);
+            await toastService.AddToastAsync(new TabBlazor.ToastModel { Title = $"{selectedIcons.Count}' copied to clipboard", Options = new TabBlazor.ToastOptions { Delay = 2 } });
+        }
+
     }
 
     public class Icon
     {
         public string Name { get; set; }
         public string Elements { get; set; }
+
+        public string GetStaticProperty()
+        {
+            return $"public static string {Name} {"{"} get => @\"{Elements}\"; {"}"} ";
+        }
     }
 
 }
--- docs/Tabler.Docs/Icons/DemoIcons.cs
+++ docs/Tabler.Docs/Icons/DemoIcons.cs
@@ -2,7 +2,6 @@
 {
     public static class DemoIcons
     {
-
         public static string _2fa { get => @"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><path d='M7 16h-4l3.47 -4.66a2 2 0 1 0 -3.47 -1.54' /><path d='M10 16v-8h4' /><line x1='10' y1='12' x2='13' y2='12' /><path d='M17 16v-6a2 2 0 0 1 4 0v6' /><line x1='17' y1='13' x2='21' y2='13' />"; }
         public static string _3d_cube_sphere { get => @"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><path d='M6 17.6l-2 -1.1v-2.5' /><path d='M4 10v-2.5l2 -1.1' /><path d='M10 4.1l2 -1.1l2 1.1' /><path d='M18 6.4l2 1.1v2.5' /><path d='M20 14v2.5l-2 1.12' /><path d='M14 19.9l-2 1.1l-2 -1.1' /><line x1='12' y1='12' x2='14' y2='10.9' /><line x1='18' y1='8.6' x2='20' y2='7.5' /><line x1='12' y1='12' x2='12' y2='14.5' /><line x1='12' y1='18.5' x2='12' y2='21' /><path d='M12 12l-2 -1.12' /><line x1='6' y1='8.6' x2='4' y2='7.5' />"; }
         public static string A_b { get => @"<path stroke='none' d='M0 0h24v24H0z' fill='none' /><path d='M3 16v-5.5a2.5 2.5 0 0 1 5 0v5.5m0 -4h-5' /><line x1='12' y1='6' x2='12' y2='18' /><path d='M16 16v-8h3a2 2 0 0 1 0 4h-3m3 0a2 2 0 0 1 0 4h-3' />"; }
