aae00592ac98f967b7cdfce930336bdc46cb4858
Joakim Dangården <Joakim.Dangarden@swegon.com>
2021-10-01 23:18:55 +0200

    fixed servcie registration

--- docs/Tabler.Docs.Wasm/Program.cs
+++ docs/Tabler.Docs.Wasm/Program.cs
@@ -19,11 +19,9 @@ namespace Tabler.Docs.Wasm
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
-            // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddHttpClient("Local", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
             builder.Services.AddHttpClient("GitHub", client => client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("TabBlazor", "1")));
-            builder.Services.AddTabler();
+            builder.Services.AddDocs();
             builder.Services.AddScoped<ICodeSnippetService, GitHubSnippetService>();
             await builder.Build().RunAsync();
         }
