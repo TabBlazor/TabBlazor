d1732802bb83f9b070dcf4500a105e353df442ad
Joakim Dangården <Joakim.Dangarden@swegon.com>
2021-10-02 11:36:09 +0200

    more navbar work

--- src/TabBlazor/Components/Navbars/Navbar.razor.cs
+++ src/TabBlazor/Components/Navbars/Navbar.razor.cs
@@ -1,18 +1,39 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TabBlazor
 {
-    public partial class Navbar : TablerBaseComponent
+    public partial class Navbar : TablerBaseComponent, IDisposable
     {
+        [Inject] private NavigationManager navigationManager { get; set; }
         [Parameter] public bool Darkmode { get; set; }
         [Parameter] public NavbarDirection Direction { get; set; }
-        protected string HtmlTag => "aside";
+        protected string HtmlTag => "div";
         public bool IsExpanded = true;
+
+        private List<NavbarMenuItem> navbarItems = new();
+
+        protected override void OnInitialized()
+        {
+            navigationManager.LocationChanged += LocationChanged;
+            base.OnInitialized();
+        }
+
+        private void LocationChanged(object sender, LocationChangedEventArgs e)
+        {
+            if (Direction == NavbarDirection.Horizontal)
+            {
+                CloseAll();
+            }
+        }
 
         protected override string ClassNames => ClassBuilder
               .Add("navbar navbar-expand-md")
               .AddIf("navbar-dark", Darkmode)
-            .AddIf("navbar-light", !Darkmode)
+              .AddIf("navbar-light", !Darkmode)
               .AddIf("navbar-vertical", Direction == NavbarDirection.Vertical)
               .ToString();
 
@@ -21,7 +42,36 @@ namespace TabBlazor
             IsExpanded = !IsExpanded;
         }
 
-        
+        public void CloseAll()
+        {
+            foreach (var item in navbarItems.Where(e => e.IsTopMenuItem))
+            {
+                item.CloseDropdown();
+            }
+
+            StateHasChanged();
+        }
+
+        public void AddNavbarMenuItem(NavbarMenuItem item)
+        {
+            if (!navbarItems.Contains(item))
+            {
+                navbarItems.Add(item);
+            }
+        }
+
+        public void RemoveNavbarMenuItem(NavbarMenuItem item)
+        {
+            if (navbarItems.Contains(item))
+            {
+                navbarItems.Remove(item);
+            }
+        }
+
+        public void Dispose()
+        {
+            navigationManager.LocationChanged -= LocationChanged;
+        }
     }
 
 }
--- src/TabBlazor/Components/Navbars/NavbarMenuItem.razor.cs
+++ src/TabBlazor/Components/Navbars/NavbarMenuItem.razor.cs
@@ -1,18 +1,21 @@
 ﻿using Microsoft.AspNetCore.Components;
+using System;
 
 namespace TabBlazor
 {
-    public partial class NavbarMenuItem : TablerBaseComponent
+    public partial class NavbarMenuItem : TablerBaseComponent, IDisposable
     {
         [CascadingParameter(Name = "Navbar")] Navbar Navbar { get; set; }
         [CascadingParameter(Name = "Parent")] NavbarMenuItem ParentMenuItem { get; set; }
-        
+
         [Parameter] public string Href { get; set; }
         [Parameter] public string Text { get; set; }
         [Parameter] public RenderFragment MenuItemIcon { get; set; }
         [Parameter] public RenderFragment SubMenu { get; set; }
         [Parameter] public bool Expanded { get; set; }
         [Parameter] public bool Expandable { get; set; } = true;
+
+        public bool IsTopMenuItem => ParentMenuItem == null;
 
         protected string HtmlTag => "li";
         protected bool isExpanded;
@@ -23,21 +26,39 @@ namespace TabBlazor
         protected override void OnInitialized()
         {
             isExpanded = Expanded;
+            Navbar?.AddNavbarMenuItem(this);
         }
 
         private bool isDropEnd => Navbar.Direction == NavbarDirection.Horizontal && ParentMenuItem?.IsDropdown == true;
 
         protected override string ClassNames => ClassBuilder
             .Add("nav-item")
-                  .Add("cursor-pointer")
+            .Add("cursor-pointer")
             .AddIf("dropdown", IsDropdown && !isDropEnd)
             .AddIf("dropend", IsDropdown && isDropEnd)
             .ToString();
 
-
-        protected void ToogleDropdown()
+        public void CloseDropdown()
         {
-            isExpanded = !isExpanded;
+            isExpanded = false;
+        }
+
+        public void ToogleDropdown()
+        {
+            var expand = !isExpanded;
+
+            if (expand && IsTopMenuItem)
+            {
+                Navbar.CloseAll();
+            }
+
+            isExpanded = expand;
+
+        }
+
+        public void Dispose()
+        {
+            Navbar?.RemoveNavbarMenuItem(this);
         }
     }
 }
