ead9597bafa7a35dfd4605a05b13f0d600e66579
Joakim Dangården <Joakim.Dangarden@swegon.com>
2022-09-13 14:54:06 +0200

    Added Data Grid Added Status

---
+++ src/TabBlazor/Components/DataGrid/DataGrid.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TabBlazor
+{
+    public partial class DataGrid : TablerBaseComponent
+    {
+     
+        protected override string ClassNames => ClassBuilder
+            .Add("datagrid")
+            .Add(BackgroundColor.GetColorClass("bg"))
+            .Add(TextColor.GetColorClass("text"))
+            .ToString();
+    }
+}
---
+++ src/TabBlazor/Components/Statuses/StatusDot.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TabBlazor
+{
+
+    public partial class StatusDot : TablerBaseComponent
+    {
+        [Parameter] public bool Animate { get; set; }
+
+
+        protected override string ClassNames => ClassBuilder
+            .Add("status-dot")
+            .AddIf(BackgroundColor.GetColorClass("status", ColorType.Default), BackgroundColor != TablerColor.Default)
+            .AddIf("status-dot-animated", Animate)
+            .AddIf("cursor-pointer", OnClick.HasDelegate)
+            .ToString();
+    }
+}
