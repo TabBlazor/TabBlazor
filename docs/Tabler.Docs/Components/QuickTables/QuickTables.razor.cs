e41f24594683ca0a6e0c05189488e4da735ed133
Magnus Ahlberg <magnus.ahlberg@swegon.com>
2022-11-14 13:33:01 +0100

    Added so that sqlite can work in wasm, maybe?

---
+++ docs/Tabler.Docs/Components/QuickTables/QuickTables.razor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace Tabler.Docs.Components.QuickTables;
+
+public partial class QuickTables
+{
+    [Inject]public IServiceProvider Provider { get; set; }
+    
+    protected override void OnInitialized()
+    {
+        SeedData.EnsureSeeded(Provider);
+    }
+}
