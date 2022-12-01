2e6fa4f1ccfe7d7539472db9de68a40c30cf299c
Magnus Ahlberg <magnus.ahlberg@swegon.com>
2022-12-01 14:21:47 +0100

    Added Accordion component

---
+++ src/TabBlazor/Components/Accordions/Accordion.razor.cs
@@ -0,0 +1,25 @@
+﻿namespace TabBlazor;
+
+public partial class Accordion : TablerBaseComponent
+{
+    private List<AccordionItem> Items { get; set; } = new();
+
+    public void AddAccordionItem(AccordionItem item)
+    {
+        Items.Add(item);
+        StateHasChanged();
+    }
+
+    private void SetExpanded(AccordionItem item)
+    {
+        var oldExpanded = item.Expanded;
+        foreach (var accordionItem in Items)
+        {
+            accordionItem.Expanded = false;
+        }
+
+        item.Expanded = !oldExpanded;
+        
+        StateHasChanged();
+    }
+}
---
+++ src/TabBlazor/Components/Accordions/AccordionItemrazor.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace TabBlazor;
+
+public partial class AccordionItem : TablerBaseComponent
+{
+    [CascadingParameter(Name = "Accordion")] public Accordion Accordion { get; set; }
+    [Parameter] public string Title { get; set; }
+    public bool Expanded { get; set; }
+
+    protected override void OnInitialized()
+    {
+        Accordion.AddAccordionItem(this);
+    }
+}
