32bd0a8946954c9f07b3638e5498caf5f6a7484b
Magnus Ahlberg <magnus.ahlberg@swegon.com>
2022-12-02 08:23:26 +0100

    Added an option for multiple open accordions

---
+++ src/TabBlazor/Components/Accordions/AccordionItem.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace TabBlazor;
+
+public class AccordionItem : TablerBaseComponent
+{
+    [CascadingParameter(Name = "Accordion")]
+    public Accordion Accordion { get; set; }
+
+    [Parameter] public string Title { get; set; }
+    [Parameter] public RenderFragment TitleTemplate { get; set; }
+    [Parameter] public bool Expanded { get; set; }
+
+    protected override void OnInitialized()
+    {
+        Accordion.AddAccordionItem(this);
+    }
+}
