d5cf35fa10e4daa2e3484b7abc8df1ff7998af12
Magnus Ahlberg <magnus.ahlberg@swegon.com>
2023-12-15 11:44:19 +0100

    Onclickoutside for offcanvas

--- src/TabBlazor/Components/Offcanvas/OffcanvasOptions.cs
+++ src/TabBlazor/Components/Offcanvas/OffcanvasOptions.cs
@@ -1,11 +1,14 @@
-﻿
-namespace TabBlazor
+﻿namespace TabBlazor
 {
     public class OffcanvasOptions
     {
         public bool Backdrop { get; set; } = true;
+        public bool CloseOnClickOutside { get; set; } = false;
+        public string WrapperCssClass { get; set; }
         public OffcanvasPosition Position { get; set; }
+        public bool CloseOnEsc { get; set; } = false;
     }
+
     public enum OffcanvasPosition
     {
         Start = 0,
@@ -13,5 +16,4 @@ namespace TabBlazor
         Top = 2,
         Bottom = 3
     }
-
 }
