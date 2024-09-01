2e2b3c5f6286547ae1896943435415372fad21f6
Magnus Ahlberg <magnus.ahlberg@swegon.com>
2024-09-01 13:28:28 +0200

    Add AssemblyScanFilter to TablerOptions
    Introduced a new property `AssemblyScanFilter` in the `TablerOptions` class to specify custom assembly scanning logic. This change allows for more flexible and targeted assembly scanning, particularly useful when dealing with flags in the application. Updated related extensions to utilize this new option.

--- src/TabBlazor/Components/Flags/FlagService.cs
+++ src/TabBlazor/Components/Flags/FlagService.cs
@@ -1,61 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
+using Microsoft.Extensions.Options;
 
+namespace TabBlazor;
 
-namespace TabBlazor
+public class FlagService
 {
-    public class FlagService
+    public FlagService(IOptions<TablerOptions> options)
     {
-
-        private List<GeneratedFlag> generatedFlags = new();
-
-        public List<GeneratedFlag> AllFlags => generatedFlags;
-        public List<IFlagType> countryFlagTypes { get; set; }
-
-
-        public IFlagType GetFlagType(int numericCode)
+        foreach (var a in options.Value.AssemblyScanFilter())
         {
-            return countryFlagTypes.FirstOrDefault(e => e.Country.Numeric == numericCode);
-        }
-
-        public IFlagType GetFlagType(string countryCode)
-        {
-            if (string.IsNullOrWhiteSpace(countryCode)) { return null; }
-
-            if (countryCode.Length == 2)
+            foreach (var t in a.GetTypes())
             {
-                return countryFlagTypes.FirstOrDefault(e => e.Country.Alpha2.ToLower() == countryCode.ToLower());
-            }
-
-            if (int.TryParse(countryCode, out var numeric))
-            {
-                return GetFlagType(numeric);
-            }
-
-            return countryFlagTypes.FirstOrDefault(e => e.Country?.Alpha3.ToLower() == countryCode.ToLower());
-        }
-
-        public FlagService()
-        {
-            foreach (Assembly a in AppDomain.CurrentDomain.GetAssemblies())
-            {
-                foreach (Type t in a.GetTypes())
+                foreach (var prop in t.GetProperties(BindingFlags.Static | BindingFlags.Public)
+                             .Where(e => e.PropertyType == typeof(IFlagType)))
                 {
-                    foreach (var prop in t.GetProperties(BindingFlags.Static | BindingFlags.Public).Where(e => e.PropertyType == typeof(IFlagType)))
-                    {
-                        generatedFlags.Add(new GeneratedFlag { Name = prop.Name, FlagType = (IFlagType)prop.GetValue(null) });
-                    }
+                    GeneratedFlags.Add(
+                        new() { Name = prop.Name, FlagType = (IFlagType)prop.GetValue(null) });
                 }
             }
-
-            countryFlagTypes = generatedFlags.Select(e => e.FlagType).Where(f => f.Country != null).ToList();
         }
+
+        CountryFlagTypes = GeneratedFlags.Select(e => e.FlagType).Where(f => f.Country != null).ToList();
     }
 
-  
+    private List<GeneratedFlag> GeneratedFlags { get; set; } = [];
+    private List<IFlagType> CountryFlagTypes { get; set; }
+    public List<GeneratedFlag> AllFlags => GeneratedFlags;
+    
 
+    public IFlagType GetFlagType(int numericCode)
+    {
+        return CountryFlagTypes.FirstOrDefault(e => e.Country.Numeric == numericCode);
+    }
+
+    public IFlagType GetFlagType(string countryCode)
+    {
+        if (string.IsNullOrWhiteSpace(countryCode))
+        {
+            return null;
+        }
+
+        if (countryCode.Length == 2)
+        {
+            return CountryFlagTypes.FirstOrDefault(e => e.Country.Alpha2.ToLower() == countryCode.ToLower());
+        }
+
+        if (int.TryParse(countryCode, out var numeric))
+        {
+            return GetFlagType(numeric);
+        }
+
+        return CountryFlagTypes.FirstOrDefault(e => e.Country?.Alpha3.ToLower() == countryCode.ToLower());
+    }
 }
