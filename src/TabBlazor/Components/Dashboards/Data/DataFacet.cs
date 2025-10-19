a80d13761be8e5dbcf39fd1c8db63f480b03f3d7
Joakim Dangården <Joakim.Dangarden@swegon.com>
2025-10-19 12:58:06 +0200

    Added Option to sort DashboardnFacet filters

--- src/TabBlazor/Components/Dashboards/Data/DataFacet.cs
+++ src/TabBlazor/Components/Dashboards/Data/DataFacet.cs
@@ -6,16 +6,24 @@ namespace TabBlazor.Dashboards
     public class DataFacet<TItem> where TItem : class
     {
         public string Name { get; set; }
+
         public List<FacetFilter<TItem>> Filters { get; set; } = new();
         public bool IsActive => Filters.Any(e => e.Active);
 
+
+     
     }
+
+
+
+
 
     public class FacetFilter<TItem> where TItem : class
     {
         public DataFilter<TItem> Filter { get; set; }
         public List<TItem> Items { get; set; }
         public IEnumerable<TItem> FilteredItems { get; set; }
+
         public bool Active { get; set; }
         public int CountAll { get; set; }
         public Func<FacetFilter<TItem>, string> FilterLabel { get; set; }
